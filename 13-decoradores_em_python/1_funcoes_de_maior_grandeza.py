ce52043761c39d105463be39554a7c27bc7292e4
Carlene.Pc_trabalho <carlenepaula@gmail.com>
2021-09-14 18:10:34 -0300

    inic_sec_13

---
+++ 13-decoradores_em_python/1_funcoes_de_maior_grandeza.py
@@ -0,0 +1,97 @@
+"""
+*** Funções de maior grandeza - higher order function -HOF
+O que isso significa?
+
+Quando uma linguagem de programação suporta HOF, indica que podemos ter funções que retornam outras
+funções como resultado. Ou mesmo que podemos passar funções como argumento para outras funções, e até mesmo
+criar variáveis do tipo de funções nos nossos programas.
+
+# OBS: isso já foi utilizado na seção de funções
+
+
+Em Python, as funções são cidadãos de primeira classe (first class functions)
+
+
+# Exemplo - Definimos as funções
+
+
+def somar(a, b):
+    return a+b
+
+
+def diminuir(a, b):
+    return a-b
+
+
+def multiplicar(a, b):
+    return a*b
+
+
+def dividir(a, b):
+    return a/b
+
+
+def calcular(num1, num2, func):
+    return func(num1, num2)
+
+
+# Testando as funções
+print(calcular(4, 3, somar))
+print(calcular(4, 3, diminuir))
+print(calcular(4, 3, multiplicar))
+print(calcular(4, 3, dividir))
+
+*** Nested functions - ou funções aninhadas
+Em Python, também podemos ter funções dentro de funções, que são conhecidas por nested functions
+ou inner functions (funções internas)
+
+
+# Exemplo de nested functions
+
+from random import choice
+
+
+def cumprimento(pessoa):
+    def humor():
+        return choice(('E ai?', 'Suma daqui!', 'Gosto muito de você!'))
+    return humor() + pessoa
+
+
+print(cumprimento('Felicity'))
+
+print(cumprimento('Felicity'))
+
+
+# Outro exemplo  - Retornando funções de outras funções
+from random import choice
+
+
+
+def faz_me_rir():
+    def rir():
+        return choice(('hahahahahaha', 'kkkkkkkkkkkk','yayayayayaya'))
+    return rir()
+
+
+rindo = faz_me_rir()
+print(rindo)
+
+Inner functions (funções internas/nested functions) podem acessar o escopo de funções mais externas
+"""
+
+from random import choice
+
+
+def faz_me_rir_novamente(pessoa):
+    def dando_risada():
+        risada = choice(('hahahahahaha', 'kkkkkkkkkkkk','yayayayayaya'))
+        return f'{risada} {pessoa}'
+    return dando_risada
+
+
+# teste
+rindo = faz_me_rir_novamente('Fernanda')
+
+print(rindo())
+print(rindo())
+print(rindo())
---
+++ 13-decoradores_em_python/decoradores.py
@@ -0,0 +1,4 @@
+"""
+Decoradores
+
+"""
